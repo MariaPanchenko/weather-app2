2c71416950babbc4e5395867d08af5dcd6fa03a6
MariaPanchenko <marysa2502@gmail.com>
2022-07-11 23:05:30 +0200

    add search

--- script.js
+++ script.js
@@ -33,13 +33,31 @@ let month = months[now.getMonth()];
 
 time.innerHTML = `${hours}:${minutes}-${day},${date} ${month}`;
 
-// search
+function displayWeather(response) {
+  document.querySelector('#city').innerHTML = response.data.name;
+  document.querySelector('#temperature').innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
 function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector('#search-input');
+  let apiKey = '7b33e98fb3b0406841a50cf97f2e248a';
+  let city = document.querySelector('#search-input').value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(displayWeather);
 
-  let h2 = document.querySelector('h2');
-  h2.innerHTML = `${searchInput.value}`;
+  // let searchInput = document.querySelector('#search-input');
+
+  // let h2 = document.querySelector('h2');
+  // h2.innerHTML = `${searchInput.value}`;
 }
 let form = document.querySelector('#search-form');
 form.addEventListener('submit', search);
+
+function showTemperature(response) {
+  let h1 = document.querySelector('h1');
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `${temperature}°C`;
+  h2.innerHTML = `${city}`;
+}
